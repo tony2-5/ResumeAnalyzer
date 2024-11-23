00ca30e68b0720cbb2f91b1f05125066f98a1471
tony2-5 <antdvory@gmail.com>
2024-11-23 15:29:22 -0500

    test/refactor: completed task 4 tests, refactored python modules

--- backend/routers/register.py
+++ backend/routers/register.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, HTTPException
 from sqlalchemy import select
-from models import users
-from schemas import UserCreate, UserResponse
-from database import database
-from auth import getPasswordHash
+from backend.models import users
+from backend.schemas import UserCreate
+from backend.database import database
+from backend.auth import getPasswordHash
 
 router = APIRouter()
 
-@router.post("/register", response_model=UserResponse, status_code=201)
+@router.post("/register", status_code=201)
 async def registerUser(user: UserCreate):
     """
     Registering new user.
@@ -29,4 +29,4 @@ async def registerUser(user: UserCreate):
     )
     await database.execute(insertQuery)
 
-    return UserResponse(email=user.email, username=user.username)
+    return  { "message": "User registered." }
